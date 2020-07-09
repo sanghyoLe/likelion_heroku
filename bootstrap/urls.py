4a3c820fdcfc94c2da2300d4d956f80ed46e5cc3
SangHyo <gilsport1@gmail.com>
2020-07-09 09:20:27 +0900

    change bootstrap menu style

---
+++ bootstrap/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('',views.bootstrap,name="bootstrap"),
+    path('ui/',views.ui,name="ui"),
+    path('use_ui/',views.use_ui,name="use_ui"),
+    path('make_navbar/',views.make_navbar,name="make_navbar"),
+    path('breakpoint/',views.breakpoint,name="breakpoint"),
+    path('dark_light/',views.dark_light,name="dark_light"),
+    path('grid/',views.grid,name="grid"),
+    path('row_col/',views.row_col,name="row_col"),
+    path('column/',views.column,name="column"),
+    path('flex_desine/',views.flex_desine,name="flex_desine"),
+    path('bootstrap_custom/',views.bootstrap_custom,name="bootstrap_custom"),
+      
+]
--- page/urls.py
+++ page/urls.py
@@ -14,22 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import bootstrap.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',bootstrap.views.home,name="home"),
     path('notyet/',bootstrap.views.notyet,name="notyet"),
-    path('bootstarp/',bootstrap.views.bootstrap,name="bootstrap"),
-    path('bootstrap/ui',bootstrap.views.ui,name="ui"),
-    path('bootstrap/use_ui',bootstrap.views.use_ui,name="use_ui"),
-    path('bootstrap/make_navbar',bootstrap.views.make_navbar,name="make_navbar"),
-    path('bootstrap/breakpoint',bootstrap.views.breakpoint,name="breakpoint"),
-    path('bootstrap/dark_light',bootstrap.views.dark_light,name="dark_light"),
-    path('bootstrap/grid',bootstrap.views.grid,name="grid"),
-    path('bootstrap/row_col',bootstrap.views.row_col,name="row_col"),
-    path('boostrap/column',bootstrap.views.column,name="column"),
-    path('bootstrap/flex_desine',bootstrap.views.flex_desine,name="flex_desine"),
-    path('bootstrap/bootstrap_custom',bootstrap.views.bootstrap_custom,name="bootstrap_custom"),
+    path('boostrap/',include('bootstrap.urls')),
 
 ]
