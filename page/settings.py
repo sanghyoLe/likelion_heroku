722478a13f2e0efacc854505bfd1abdec05b35b4
SangHyo <gilsport1@gmail.com>
2021-02-10 03:34:08 +0900

    ad dj_database_url

--- page/settings.py
+++ page/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,6 +130,6 @@ STATIC_ROOT = 'static'
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
 
 # settings.py # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url 
+
 db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
